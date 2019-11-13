763843074c515fa61deca0432d90af62a0820144
ry_takahashi <ryosuke_takahashi@respawn.jp>
2019-11-13 13:14:36 +0900

    テンプレートを作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,44 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>掲示板</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <header>
-    <nav class="my-navbar">
-      <a href="/" class="my-navbar-brand">掲示板</a>
-    </nav>
-  </header>
-  <main>
-    <div class="container">
-      <div class="row">
-        <div class="col col-md-4">
-          <nav class="panel panel-default">投稿</nav>
-          <div class="panel-body">
-            <a href="{{ route('posts.create') }}" class="btn btn-default btn-block">
-              投稿する
-            </a>
-          </div>
-          <div class="list-group">
-            @foreach($posts as $post)
-            <div class="card" style="width: 18rem;">
-              <div class="card-body">
-                <h5 class="card-title">{{ $post->title }}</h5>
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-4">
+        <nav class="panel panel-default">投稿</nav>
+        <div class="panel-body">
+          <a href="{{ route('posts.create') }}" class="btn btn-default btn-block">
+            投稿する
+          </a>
+        </div>
+        <div class="list-group">
+          @foreach($posts as $post)
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{ $post->title }}</h5>
+              <a href="{{ action('PostController@show', $post->id) }}">
                 <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at }}</h6>
-                <p class="card-text">{{ $post->content }}</p>
-                <a href="#" class="card-link">リンク1</a>
-                <a href="#" class="card-link">リンク2</a>
-              </div>
+              </a>
+              <p class="card-text">{{ $post->content }}</p>
+              <a href="#" class="card-link">リンク1</a>
+              <a href="#" class="card-link">リンク2</a>
             </div>
-            @endforeach
           </div>
+          @endforeach
         </div>
       </div>
     </div>
-  </main>
-</body>
-</html>
+  </div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,26 @@
-{{-- <p>{{ $post->id }}</p>
-<p>{{ $post->title }}</p>
-<p>{{ $post->content }}</p>
-<p>{{ $post->created_at }}</p> --}}
+@extends('layout')
 
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-4">
+        <nav class="panel panel-default">投稿</nav>
+        <div class="panel-body">
+          <a href="{{ route('posts.create') }}" class="btn btn-default btn-block">
+            投稿する
+          </a>
+        </div>
+        <div class="list-group">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{ $post->title }}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at }}</h6>
+              <p class="card-text">{{ $post->content }}</p>
+              <a href="#" class="card-link">リンク1</a>
+              <a href="#" class="card-link">リンク2</a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
